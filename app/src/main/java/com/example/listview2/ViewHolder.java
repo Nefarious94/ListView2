243d76ea6b58fd1f5f2c12ee9a2e21506e1ca067
sergi <sergi.f@gmail.com>
2020-02-06 16:26:18 +0100

    listview2

---
+++ app/src/main/java/com/example/listview2/ViewHolder.java
@@ -0,0 +1,9 @@
+package com.example.listview2;
+import android.view.View;
+import android.widget.RatingBar;
+class ViewHolder {
+    RatingBar rate=null;
+    ViewHolder(View base) {
+        this.rate=(RatingBar)base.findViewById(R.id.rate);
+    }
+}
